421a4c4c8c8a837a7c9d2e69593c32e9157bba20
yopa-ktc <yoparemy@gmail.com>
2023-05-18 22:51:29 +0200

    mis à jour v2 de la fonction calcul date avec l'iteration sur un tableau

--- setDate.py
+++ setDate.py
@@ -6,50 +6,59 @@ import pandas as pd
 
 
 date_actuelle = date.today()
-# print(date_actuelle-timedelta(days=30*3))
 
-# list_date = pd.read_excel("transformeDate.xlsx")
+list_date = pd.read_excel("transformeDate.xlsx")
 
-# print(list_date['Date'])
+def calculDate(list_date):
+    date_calcul = ''
+    for this_date in list_date['Date']:
+        print(this_date)
+        #ON VERIFIE LE FORMAT DU TEMPS (jour, mois, année)
+        #Création des regex :
+        #___Vérification pour les années
+        verif_year = re.findall(r"\b(\d+|un) (an|ans)\b", this_date)
+        #___Vérification pour les mois
+        verif_month = re.findall(r"\b(\d+|un) (mois)\b", this_date)
+        #___Vérification pour les semaines
+        verif_week = re.findall(r"\b(\d+|une) (semaine|semaines)\b", this_date)
+        #___Vérification pour les jours
+        verif_days = re.findall(r"\b(\d+|un) (jour|jours)\b", this_date)
 
-
-chaine = "un an"
-#ON VERIFIE LE FORMAT DU TEMPS (jour, mois, année)
-#Création des regex :
-#___Vérification pour les années
-verif_year = re.findall(r"\b(\d+|un) (an|ans)\b", chaine)
-#___Vérification pour les mois
-verif_month = re.findall(r"\b(\d+|un) (mois)\b", chaine)
-#___Vérification pour les semaines
-verif_week = re.findall(r"\b(\d+|une) (semaine|semaines)\b", chaine)
-#___Vérification pour les jours
-verif_days = re.findall(r"\b(\d+|un) (jour|jours)\b", chaine)
-    
-
-if len(verif_year) != 0:
-    try:
-        if(int(verif_year[0][0])):#Soustraction de plusieurs
-            print(date_actuelle-timedelta(days=365*int(verif_year[0][0])))
-    except ValueError:#Soustraction d'un seul
-        print(date_actuelle-timedelta(days=365))
-        
-elif len(verif_month) != 0:
-    try:
-        if(int(verif_month[0][0])):#Soustraction de plusieurs
-            print(date_actuelle-timedelta(days=30*int(verif_month[0][0])))
-    except ValueError:#Soustraction d'un seul
-        print(date_actuelle-timedelta(days=30))
-elif len(verif_week) != 0:
-    try:
-        if(int(verif_week[0][0])):#Soustraction de plusieurs
-            print(date_actuelle-timedelta(days=7*int(verif_week[0][0])))
-    except ValueError:#Soustraction d'un seul
-        print(date_actuelle-timedelta(days=7))
-elif len(verif_days) != 0:
-    try:
-        if(int(verif_days[0][0])):#Soustraction de plusieurs
-            print(date_actuelle-timedelta(days=int(verif_days[0][0])))
-    except ValueError:#Soustraction d'un seul
-        print(date_actuelle-timedelta(days=1))
-else:
-    print("aucune correspondance")
+        if len(verif_year) != 0:
+            try:
+                if(int(verif_year[0][0])):#Soustraction de plusieurs années
+                    print(date_actuelle-timedelta(days=365*int(verif_year[0][0])))
+                    date_calcul = date_actuelle-timedelta(days=365*int(verif_year[0][0]))
+            except ValueError:#Soustraction d'un seul
+                date_calcul = date_actuelle-timedelta(days=365*int(verif_year[0][0]))
+                print(date_actuelle-timedelta(days=365))
+                
+        elif len(verif_month) != 0:
+            try:
+                if(int(verif_month[0][0])):#Soustraction de plusieurs mois
+                    print(date_actuelle-timedelta(days=30*int(verif_month[0][0])))
+                    date_calcul = date_actuelle-timedelta(days=30*int(verif_month[0][0]))
+            except ValueError:#Soustraction d'un seul
+                date_calcul = date_actuelle-timedelta(days=30*int(verif_month[0][0]))
+                print(date_actuelle-timedelta(days=30))
+        elif len(verif_week) != 0:
+            try:
+                if(int(verif_week[0][0])):#Soustraction de plusieurs semaines
+                    print(date_actuelle-timedelta(days=7*int(verif_week[0][0])))
+                    date_calcul = date_actuelle-timedelta(days=7*int(verif_week[0][0]))
+            except ValueError:#Soustraction d'un seul
+                date_actuelle-timedelta(days=7*int(verif_month[0][0]))
+                print(date_actuelle-timedelta(days=7))
+        elif len(verif_days) != 0:
+            try:
+                if(int(verif_days[0][0])):#Soustraction de plusieurs jours
+                    date_calcul = date_actuelle-timedelta(days=int(verif_days[0][0]))
+                    print(date_actuelle-timedelta(days=int(verif_days[0][0])))
+            except ValueError:#Soustraction d'un seul
+                date_calcul = date_actuelle-timedelta(days=int(verif_days[0][0]))
+                print(date_actuelle-timedelta(days=1))
+        else:
+            print("aucune correspondance")
+    return date_calcul
+            
+calculDate(list_date)
