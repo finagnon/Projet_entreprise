4b92cccc2160f4e3d156647b4cb7ac1944d0559a
yopa-ktc <yoparemy@gmail.com>
2023-05-26 19:20:35 +0200

    recupération des infos

--- Streamlit/streamlit_viz.py
+++ Streamlit/streamlit_viz.py
@@ -26,8 +26,6 @@ ax1.axis('equal')
 plt.tight_layout()
 st.pyplot(fig)
 
-
-
 # df = pd.DataFrame(
 #     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
 #     columns=['lat', 'lon'])
@@ -35,14 +33,11 @@ st.pyplot(fig)
 # st.map(df)
 
 
-
-
-
 DATE_COLUMN = 'Date'
 
 @st.cache_data
 def load_data(nrows):
-    data = pd.read_csv('C:/Users/sylva/OneDrive/Bureau/scrap_project_MD4/Projet_entreprise/Data/Île-de-France_POLE_EMPLOI_copie.csv', nrows=nrows, sep=';')
+    data = pd.read_csv('../Data/Île-de-France_POLE_EMPLOI_copie.csv', nrows=nrows, sep=';')
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
@@ -71,7 +66,7 @@ st.pyplot(fig)
 
 
 # Charger le second dataframe avec les colonnes "Ville", "Nombre_service" et "Periode"
-df = pd.read_csv("C:/Users/sylva/OneDrive/Bureau/scrap_project_MD4/Projet_entreprise/Data/Île-de-France_Nbre_service.csv", sep=';')
+df = pd.read_csv("../Data/ile-de-France_Nbre_service.csv", sep=';')
 
 
 # # Géocoder les noms de villes en utilisant geopandas
@@ -129,3 +124,35 @@ plt.show()
 
 # # Afficher le graphique
 # plt.show()
+
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
