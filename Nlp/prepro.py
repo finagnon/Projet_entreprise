0d7abe0365be2a68bb760839eb1c5da1b8e728aa
finagnon <fiacreyehossou@gmail.com>
2023-06-02 03:32:04 +0200

    update NLP Streamlit

--- Nlp/prepro.py
+++ Nlp/prepro.py
@@ -1,10 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 22 11:34:50 2023
+
+@author: finagnon
+"""
+
 from transformers import pipeline
 #import re
 from tqdm import tqdm
 import pandas as pd
 import nltk
+import spacy
+from spacy.lang.fr.stop_words import STOP_WORDS
 nltk.download('punkt')
 import neattext.functions as nfx
+import re
+
+
 
 
 #Fonction de lecture du fichier
@@ -12,6 +25,8 @@ def  liref(fichier):
 
         df = pd.read_csv(fichier, sep=';')
         return df
+
+
 
 """
 def nlp_pipeline(text):
@@ -36,135 +51,41 @@ def nlp_pipeline(text):
     return text
 """
 
+
+
 def process_data(df):
         
         
         avs,dt_publi=[],[]
     # Fonction de traitement de données
                 
-        df["Description"]=df["Description"].apply(nfx.remove_userhandles)
-        df["Description"]=df["Description"].apply(nfx.remove_phone_numbers)
-        df["Description"]=df["Description"].apply(nfx.remove_emails)
-        df["Description"]=df["Description"].apply(nfx.remove_stopwords)
-        df["Description"]=df["Description"].apply(nfx.remove_special_characters)
+        df["Commentaires"]=df["Commentaires"].apply(nfx.remove_userhandles)
+        df["Commentaires"]=df["Commentaires"].apply(nfx.remove_phone_numbers)
+        df["Commentaires"]=df["Commentaires"].apply(nfx.remove_emails)
+        #df["Commentaires"]=df["Commentaires"].apply(nfx.remove_stopwords)
+        df["Commentaires"]= df["Commentaires"].apply(lambda x: x.lower())
 
-        
+        df["Commentaires"] = df["Commentaires"].map(lambda x : re.sub(r'[^a-zA-Z ]', '', x))
+
+        df["Commentaires"]= df["Commentaires"].apply(lambda x : nfx.remove_stopwords (x))
+
+        df["Commentaires"]=df["Commentaires"].apply(nfx.remove_special_characters)
+        df["Commentaires"]=df["Commentaires"].apply(nfx.remove_emojis)
+        df["Commentaires"]=df["Commentaires"].apply(nfx.remove_hashtags)
+        df["Commentaires"]=df["Commentaires"].apply(nfx.remove_html_tags)
+        df["Commentaires"]=df["Commentaires"].apply(nfx.remove_multiple_spaces)
+        #df["Description"]=df["Commentaires"].apply(nfx.remove_multiple_spaces)
+
+        pattern = r"\b(\w+(?:-\w+)*)$"
+        df["Ville"] = df["Localisation"].str.extract(pattern, expand=False)
         
         return df
-"""
-def nlp_function(data):
-         
-        #df_zip =dict(zip(avs, dt_publi))
-      #  processed = [avs,dt_publi,df_zip]
-        Data =data[0]
-        df_z = data[2]
-        av =[]
-        score = []
-        tp = []
-        publication =[]
-        analyzer = pipeline(
-    task='text-classification',
-    model="cmarkea/distilcamembert-base-sentiment",
-    tokenizer="cmarkea/distilcamembert-base-sentiment"
-)
-        for avi in tqdm(Data):  
-                if len(avi) < 700:
-                        data["TYPE"] = data["Description"].apply(lambda x, )
-                        result = analyzer(avi, return_all_scores=True)
-                        ab = result[0][0]['score'] + result[0][1]['score']
-                        ab1 = result[0][2]['score']
-                        ab2 = result[0][3]['score'] + result[0][4]['score']
-                        if ab > ab1 and ab > ab2:
-                                print("{}===================> {}".format(avi, ab))
-                                av.append(avi)
-                                score.append(ab)
-                                tp.append('Negatif')
-                                publication.append(df_z[avi])
-                                
-                  
-                        elif ab > ab1 and ab < ab2:
-                                print("{}===================> {}".format(avi, ab2))
-                                av.append(avi)
-                                score.append(ab2)
-                                tp.append('Positif')
-                                publication.append(df_z[avi])
 
-                        elif ab1 > ab and ab1 > ab2: 
-                                        print("{}===================> {}".format(avi, ab1))
-                                        av.append(avi)
-                                        score.append(ab1)
-                                        tp.append('Neutre')
-                                        publication.append(df_z[avi])
+#Fonction filtre
+def filter_data(data):
+    filtered_data = data[~data['Commentaires'].str.contains(r"(gouvernement|grnt|Gouvernement|Macron|président|President|Président|ministre|Ministre|Etat|état)", case=False)]
+    return filtered_data
 
-                        elif ab2 > ab and ab2 > ab1:
-                                        print("{}===================> {}".format(avi, ab2))
-                                        av.append(avi)
-                                        score.append(ab2)
-                                        tp.append('Positif')
-                                        publication.append(df_z[avi])
-        
-        
-        data1 = {"Date de publication":publication, 'AVI':av,'SCORE':score, 'TYPE':tp}
-        df = pd.DataFrame(data1)
-        df1=df.to_csv("avi.csv")
-        data2 = [df, df1]
-        return data2
-"""
 
-def nlp_function(avi):
 
-         #Negatif ='Negatif'
-         #Positif ='Positif'
-         #Neutre =' Neutre'
-        #df_zip =dict(zip(avs, dt_publi))
-      #  processed = [avs,dt_publi,df_zip]
-        #av= []
-        #score = []
-        #tp = []
-        #publication =[]
-        analyzer = pipeline(
-    task='text-classification',
-    model="cmarkea/distilcamembert-base-sentiment",
-    tokenizer="cmarkea/distilcamembert-base-sentiment"
-)
-        result = analyzer(avi, return_all_scores=True)
-        ab = result[0][0]['score'] + result[0][1]['score']
-        ab1 = result[0][2]['score']
-        ab2 = result[0][3]['score'] + result[0][4]['score']
 
-        if ab > ab1 and ab > ab2:
-                                #print("{}===================> {}".format(avi, ab))
-                                #av.append(avi)
-                                #score.append(ab)
-                                return 'Negatif'
-
-                        
-        elif ab > ab1 and ab < ab2:
-                                #print("{}===================> {}".format(avi, ab2))
-                                #av.append(avi)
-                                #score.append(ab2)
-                                return 'Positif'               
-                                
-                  
-        elif ab1 > ab and ab1 > ab2: 
-                                        #print("{}===================> {}".format(avi, ab1))
-                                        #av.append(avi)
-                                        #score.append(ab1)
-                                return 'Neutre'
-
-        elif ab2 > ab and ab2 > ab1:
-                                        #print("{}===================> {}".format(avi, ab2))
-                                        #av.append(avi)
-                                        #score.append(ab2)
-                                return 'Positif'
-              
-                                
-                                  
-        
-        """
-        data1 = {"Date de publication":publication, 'AVI':av,'SCORE':score, 'TYPE':tp}
-        df = pd.DataFrame(data1)
-        df1=df.to_csv("avi.csv")
-        data2 = [df, df1]
-        return data2
-"""
---
+++ Streamlit/api2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 22 14:27:55 2023
+
+@author: finagnon
+"""
+from transformers import pipeline
+from Nlp.show_me import * 
+import pandas as pd
+from Nlp.nlp import*
+from Nlp.prepro import *
+from tqdm import tqdm
+from Nlp.diff import *
+
+from cluster import clust
+
+
+# Page de sélection du fichier
+def select_file():
+    show_header()
+    st.title("Sélectionnez un fichier")
+    uploaded_file = st.file_uploader("Choisissez un fichier CSV", type="csv")
+    
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file, sep=',')
+        df =filter_data(df)
+        # Affichage des données sur une nouvelle page
+        show_data(df.head(10))
+        
+        # Bouton pour traiter le fichier
+        if st.button("Traiter le fichier"):
+            data_pre= process_data(df)
+            #data_pre2 = data_pre[ : 10]
+            #data_pre1 =  classify_data(data_pre2)
+            data_pre1 =  classify_data(data_pre)
+
+            df1 = negatif_avis(data_pre1)
+            df2 = positif_avis(data_pre1)
+            
+            df1["Label"] = df1['Avis'].apply(assign_label)
+            df3 = df1.groupby(["Ville","Label"]) 
+            df4 = df3['Label'].count()
+            df4.to_csv('label_ville.csv')
+            # Affichage du résultat sur une nouvelle page
+            #show_processed_data(data_pre)
+            show_classified_data(data_pre1)
+            show_processed(df1)
+            show_processed_p(df2)
