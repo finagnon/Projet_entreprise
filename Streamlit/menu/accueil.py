afb0998dd7dbfd7415b279b395ea549c7cf62663
Diouf <sylvaindiouf18@gmail.com>
2023-06-05 16:39:08 +0200

    j'ai ajouté le logo et d'autres modifications.

--- Streamlit/menu/accueil.py
+++ Streamlit/menu/accueil.py
@@ -2,11 +2,11 @@ from PIL import Image
 import streamlit as st
 
 def accueil():
-    # Charger et afficher l'image 
-    # image_pole_emploi = Image.open("../Data/Image/Logo.png")    
-    # st.image(image_pole_emploi, use_column_width=True)    
+    #Charger et afficher l'image 
+    image_pole_emploi = Image.open("../Data/Image/Logo.png")    
+    st.image(image_pole_emploi, use_column_width=True)    
     
-    st.markdown("<h1 style='color: blue;'>Tableau de Bord</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color: #000000;'>Tableau de Bord</h1>", unsafe_allow_html=True)
     st.markdown("<h2 style='color: #000000;'>Bienvenue dans Projet Entreprise</h2>", unsafe_allow_html=True)
     
     # Charger et afficher l'image 
